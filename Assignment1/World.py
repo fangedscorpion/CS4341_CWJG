28e97bd566b6be1f06490a52207f8a9f16436ca1
Jacob Zizmor <jlzizmor@wpi.edu>
2015-09-08 01:02:05 -0400

    implemented and tested getNeightbors()

--- Assignment1/World.py
+++ Assignment1/World.py
@@ -1,5 +1,6 @@
 """ World Class """
 """ Representation of all cells in the World """
+from Coord import Coord
 
 
 class World(object):
@@ -19,7 +20,7 @@ class World(object):
     # INPUT -> (file) input world
     # OUTPUT -> (list of list of Cells) formatted world
     def MakeWorld(self, file):
-    	# get all lines from input file
+        # get all lines from input file
         lines = file.readlines()
 
         # split all idv input lines into lists
@@ -27,12 +28,13 @@ class World(object):
             lines[j] = lines[j].split()
 
         # create list same size of the input grid
-        world = [[0 for x in range(len(lines[0]))] for x in range(len(lines))] 
+        world = [[0 for x in range(len(lines[0]))] for x in range(len(lines))]
 
         # assign values from lines into world list
         for k in range(0, len(lines)):
             for l in range(0, len(lines[k])):
-            	# replace 'lines[k][l]' with instanciation of Cell when Cell is defined
+                # replace 'lines[k][l]' with instanciation of Cell when Cell is
+                # defined
                 world[k][l] = lines[k][l]
 
         # return world
@@ -43,27 +45,34 @@ class World(object):
     # INPUT -> none
     # OUTPUT -> (cell) starting cell, OR (int) -1
     def getStart(self):
-    	for j in range(0, len(self.world)):
-    		for k in range(0, len(self.world[j])):
-    			# if self.world[j][k].isStart():
-    			# ^ switch boolean line when Cell is defined
-    			if self.world[j][k] == "S":
-    				return self.world[j][k]
-    	return -1
+        for j in range(0, len(self.world)):
+            for k in range(0, len(self.world[j])):
+                # if self.world[j][k].isStart():
+                # ^ switch boolean line when Cell is defined
+                if self.world[j][k] == "S":
+                    return self.world[j][k]
+        return -1
 
     # parses through world looking for the goal cell
     # if no starting cell is found, returns -1
     # INPUT -> none
     # OUTPUT -> (cell) starting cell, OR (int) -1
     def getGoal(self):
-    	for j in range(0, len(self.world)):
-    		for k in range(0, len(self.world[j])):
-    			# if self.world[j][k].isGoal():
-    			# ^ switch boolean line when Cell is defined
-    			if self.world[j][k] == "G":
-    				return self.world[j][k]
-    	return -1
+        for j in range(0, len(self.world)):
+            for k in range(0, len(self.world[j])):
+                # if self.world[j][k].isGoal():
+                # ^ switch boolean line when Cell is defined
+                if self.world[j][k] == "G":
+                    return self.world[j][k]
+        return -1
+
+    # returns a list of the 4 neighbors surrounding a given cell
+    # [N, E, S, W]
+    def getNeighbors(self, a_coord):
+        neighbors = [self.world[a_coord.getY() - 1][a_coord.getX()], self.world[a_coord.getY()]
+                     [a_coord.getX() + 1], self.world[a_coord.getY() + 1][a_coord.getX()], self.world[a_coord.getY()][a_coord.getX() - 1]]
+        return neighbors
 
 if __name__ == "__main__":
     aworld = World(open("test_board.txt", "r"))
-    print aworld.goal
+    print aworld.getNeighbors(Coord(1, 1))
