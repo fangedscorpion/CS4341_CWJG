5d47a1577f7fb12c3fa22bdc1bc5753ed2bfdbc1
Jacob Zizmor <jlzizmor@wpi.edu>
2015-09-08 17:32:15 -0400

    created and setup heuristic class

---
+++ Assignment1/Heuristic.py
@@ -0,0 +1,9 @@
+""" Class for Heuristic functions """
+
+class Heuristic(object):
+	# function is an integer representing the specific huersitic function
+	def __init__(self, function):
+		self.function = function
+
+if __name__ == "__main__":
+	a_h = Heuristic(4)
