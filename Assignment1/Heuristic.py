9a2ecd7b983a35d63fbf016606f7f40a6b1ff4b5
Jacob Zizmor <jlzizmor@wpi.edu>
2015-09-08 18:13:23 -0400

    implemented and tested heur1()

--- Assignment1/Heuristic.py
+++ Assignment1/Heuristic.py
@@ -5,5 +5,19 @@ class Heuristic(object):
 	def __init__(self, function):
 		self.function = function
 
+	# This function returns the integer representing the specific heuristic function
+	# INPUT -> none
+	# OUTPUT -> (int) function
+	def getFunction(self):
+		return self.function
+
+	# This function returns the heuristic evaluation for heuristic 1
+	# heuristic 1 is always 0
+	# INPUT -> (Cell) the cell being evaluated
+	# OUTPUT -> (int) 0
+	def heur1(self, a_cell):
+		return 0
+
 if __name__ == "__main__":
 	a_h = Heuristic(4)
+	print a_h.heur1("test cell")
