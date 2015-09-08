98ad4b014b3e2a615a17e40d5ca0dfce9a7d64eb
Jacob Zizmor <jlzizmor@wpi.edu>
2015-09-07 23:36:47 -0400

    formatted and added function header comments

--- Assignment1/Coord.py
+++ Assignment1/Coord.py
@@ -1,33 +1,40 @@
 """ A coordinate class """
+
+
 class Coord(object):
-	def __init__(self, x, y):
-		self.x = x # the x coordinate value
-		self.y = y # the y coordinate value
 
-	# overriding the equals (==) operator
-	def __eq__(self, other):
-		return (self.x == other.x) and (self.y == other.y)
+    def __init__(self, x, y):
+        self.x = x  # the x coordinate value
+        self.y = y  # the y coordinate value
 
-	# overriding the not equals (!=) operator
-	def __ne__(self, other):
-		return not self == other
+    # overriding the equals (==) operator
+    def __eq__(self, other):
+        return (self.x == other.x) and (self.y == other.y)
 
-	# returns the x coordinate
-	def getX(self):
-		return self.x
+    # overriding the not equals (!=) operator
+    def __ne__(self, other):
+        return not self == other
 
-	# returns the y coordinate
-	def getY(self):
-		return self.y
+    # returns the x coordinate
+    # INPUT -> none
+    # OUTPUT -> (int) x coordinate
+    def getX(self):
+        return self.x
+
+    # returns the y coordinate
+    # INPUT -> none
+    # OUTPUT -> (int) x coordinate
+    def getY(self):
+        return self.y
 
 if __name__ == "__main__":
-	A = Coord(1,2)
-	B = Coord(2,1)
-	C = Coord(1,2)
+    A = Coord(1, 2)
+    B = Coord(2, 1)
+    C = Coord(1, 2)
 
-	print A == B, "False"
-	print A != B, "True"
-	print A == C, "True"
+    print A == B, "False"
+    print A != B, "True"
+    print A == C, "True"
 
-	print A.getX(), "1"
-	print A.getY(), "2"
+    print A.getX(), "1"
+    print A.getY(), "2"
