13561d62945e012602630fdcefad63188c4b23fe
Jacob Zizmor <jlzizmor@wpi.edu>
2015-09-07 23:39:37 -0400

    fixed function prompt for MakeWorld()

--- Assignment1/World.py
+++ Assignment1/World.py
@@ -1,30 +1,34 @@
 """ World Class """
 """ Representation of all cells in the World """
 
+
 class World(object):
-	# file is the input world file
-	# MakeWorld parses the input file and creates a list of a list of cells
-	# self.start stores the starting cell to avoid using the function getStart() repeatidly
-	# self.goal stores the goal cell to avoid using the function getGoal() repeatidly
-	def __init__(self, file):
-		self.file = file
-		self.world = MakeWorld(self.file)
-		self.start = getStart()
-		self.goal = getGoal()
+    # file is the input world file
+    # MakeWorld parses the input file and creates a list of a list of cells
+    # self.start stores the starting cell to avoid using the function getStart() repeatidly
+    # self.goal stores the goal cell to avoid using the function getGoal()
+    # repeatidly
 
-	# parses the input file and creates the world
-	# INPUT -> (file) input world
-	# OUTPUT -> (list of list of Cells) formatted world
-	def MakeWorld(file)
-		lines = file.readlines()
+    def __init__(self, file):
+        self.file = file
+        self.world = MakeWorld(self.file)
+        self.start = getStart()
+        self.goal = getGoal()
 
-		for j in range(0, len(lines)):
-			fixed.append(lines[j].split)
+    # parses the input file and creates the world
+    # INPUT -> (file) input world
+    # OUTPUT -> (list of list of Cells) formatted world
+    def MakeWorld(self, file)
+        lines = file.readlines()
 
-		for k in range(0, len(fixed)):
-			for l in range(0, len(fixed[k])):
-				# world[l][k] = Cell(fixed[k][l])
-				world[l][k] = fixed[k][l] # this line is for debugging, replace with line above when Cell is defined
+        for j in range(0, len(lines)):
+            fixed.append(lines[j].split)
 
-		return world
+        for k in range(0, len(fixed)):
+            for l in range(0, len(fixed[k])):
+                # world[l][k] = Cell(fixed[k][l])
+                # this line is for debugging, replace with line above when Cell
+                # is defined
+                world[l][k] = fixed[k][l]
 
+        return world
