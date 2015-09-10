14482f42a15044623ec28c57832d1d1048520088
Jacob Zizmor <jlzizmor@wpi.edu>
2015-09-09 21:35:31 -0400

    formatting

--- Assignment1/TurnAction.py
+++ Assignment1/TurnAction.py
@@ -1,12 +1,13 @@
 class TurnAction(Action):
-	"""This is a more sophisticated turn Action"""
 
-	def __init__(self):
-		Action.init(self, "Turn", -1)
-		print("Debug")
+    """This is a more sophisticated turn Action"""
 
-	def calculateTurnCost(self, currentCellVal):
-		print("Define how to calculate the turn cost")
+    def __init__(self):
+        Action.init(self, "Turn", -1)
+        print("Debug")
+
+    def calculateTurnCost(self, currentCellVal):
+        print("Define how to calculate the turn cost")
 
 if __name__ == "__main__":
-	print("Something debuggy here")
+    print("Something debuggy here")
