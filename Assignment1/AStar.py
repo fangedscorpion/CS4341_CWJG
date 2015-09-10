645449c9ce55af40a43cabd596153a837b54dff9
wsbarnard <wsbarnard@wpi.edu>
2015-09-10 08:30:47 -0400

    Added some of the Bash functionality to A*

--- Assignment1/AStar.py
+++ Assignment1/AStar.py
@@ -1,6 +1,6 @@
 #!/usr/bin/env python
 
-def AStar(world, start, goal): #start and goal are nodes
+def AStar(world, start, goal): #start and goal are cells
     print "Running A*"
 
     visited = [] #nodes
@@ -18,20 +18,48 @@ def AStar(world, start, goal): #start and goal are nodes
         visited.append(current)
 
         for each neighbor in current.neighbors
-            if neighbor is in visited 
+            if (neighbor is in visited) or (not neighbor.isValid())
                 continue
 
+            localDir = findDirection(currentCell, moveCell)
+            if shouldBashHa(current, neighbor)
+                neighbor = neighbor.neighbors[FindDirecton(localDir)]
+                neighbor.actionList = 
             # action cost needs to be a function that calculates the cost of moving from 
             # a cell to the specified neighbor cell based on the orientation and movement costs
-            tentativeEstCost = current.costSoFar + actionCost(current,neighbor) + neighbor.heuristic
+            tentativeEstCost = current.costSoFar + actionCost() + neighbor.heuristic
 
             if (neighbor not in frontier) or (tentativeEstCost < estCost)
-                neighbor.parent = current
+                neighbor.parent = current #replace this by appending the action list
                 neighbor.costSoFar = current.costSoFar + actionCost(current, neighbor)
                 neighbor.estCost = tentativeEstCost
                 if neighbor not in frontier
                     frontier.append(neighbor)
     print "A* failed"
+
+def shouldBashHa(current, neighbor):
+    localDir = findDirection(currentCell, moveCell)
+    if canBash() and (neighbor.getCurrentCell().getComplexity() > 3)
+        tentCostBash = actionCost(current.getCurrentCell(), neighbor.getCurrentCell(), True) + neighbor.neighbors[localDir].heuristic
+        tentCostDrive = actionCost(current.getCurrentCell(), neighbor.getCurrentCell(), False) + neighbor.heuristic
+        if tentCostBash < tentCostDrive
+            return True
+    else
+        return False
+
+
+
+
+
+def actionCost(currentCell, moveCell, isBash):
+    localDir = findDirection(currentCell, moveCell)
+    turningCost = turningCalc(localDir)
+
+    if isBash
+        return turningCost + moveCell.neighbors[localDir].getComplexity() + 3
+    else
+        return turningCost + moveCell.getComplexity()
+
 
 
                 
