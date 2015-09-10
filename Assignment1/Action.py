dbb287205b9e5cf8b3b63bf2e400323c6765bf4c
Jacob Zizmor <jlzizmor@wpi.edu>
2015-09-09 21:35:14 -0400

    formatting and spacing

--- Assignment1/Action.py
+++ Assignment1/Action.py
@@ -1,22 +1,24 @@
 class Action:
-	"""This class is a simple represetation of the various actions the robot can take"""
-	actionName = "" #A String name
-	timeCost  = -1 #The time cost of the action
 
-	def __init__(self, actionName, timeCost):
-		self.actionName = actionName
-		self.timeCost = timeCost
+    """This class is a simple represetation of the various actions the robot can take"""
+    actionName = ""  # A String name
+    timeCost = -1  # The time cost of the action
 
-	def getTimeCost(self):
-		return self.timeCost
+    def __init__(self, actionName, timeCost):
+        self.actionName = actionName
+        self.timeCost = timeCost
 
-	def getActionName(self):
-		return self.actionName
-	def __repr__(self):
-		string = "Name: " + str(self.actionName) + ". Cost: " + str(self.timeCost)
-		return string
+    def getTimeCost(self):
+        return self.timeCost
+
+    def getActionName(self):
+        return self.actionName
+
+    def __repr__(self):
+        string = "Name: " + \
+            str(self.actionName) + ". Cost: " + str(self.timeCost)
+        return string
 
 if __name__ == "__main__":
-	actionDebug = Action("Debug Action", 30)
-	print actionDebug
-	
+    actionDebug = Action("Debug Action", 30)
+    print actionDebug
