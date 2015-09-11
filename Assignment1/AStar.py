243fec86d9cd75913dc6da4c5bc3e57ef33cbd20
Chas Frick <cjfrick@wpi.edu>
2015-09-11 13:00:29 -0400

    Updated the main file for astar to be the main and AStarSearch is the other file

--- Assignment1/AStar.py
+++ Assignment1/AStar.py
@@ -1,65 +1,22 @@
-#!/usr/bin/env python
+import sys
+from World import World
+from AStar2 import AStar2
 
-def AStar(world, start, goal): #start and goal are cells
-    print "Running A*"
-
-    visited = [] #nodes
-    frontier = [] #nodes
-    pathList = [] #nodes
-
-    frontier.append(start)
-
-    while frontier is not empty 
-        current = findLowestCost(frontier)
-        if(current is goal)
-            return reconstructPath() #this will be a thing
-
-        frontier.remove(current)
-        visited.append(current)
-
-        for each neighbor in current.neighbors
-            if (neighbor is in visited) or (not neighbor.isValid())
-                continue
-
-            localDir = findDirection(currentCell, moveCell)
-            if shouldBashHa(current, neighbor)
-                neighbor = neighbor.neighbors[FindDirecton(localDir)]
-                neighbor.actionList = 
-            # action cost needs to be a function that calculates the cost of moving from 
-            # a cell to the specified neighbor cell based on the orientation and movement costs
-            tentativeEstCost = current.costSoFar + actionCost() + neighbor.heuristic
-
-            if (neighbor not in frontier) or (tentativeEstCost < estCost)
-                neighbor.parent = current #replace this by appending the action list
-                neighbor.costSoFar = current.costSoFar + actionCost(current, neighbor)
-                neighbor.estCost = tentativeEstCost
-                if neighbor not in frontier
-                    frontier.append(neighbor)
-    print "A* failed"
-
-def shouldBashHa(current, neighbor):
-    localDir = findDirection(currentCell, moveCell)
-    if canBash() and (neighbor.getCurrentCell().getComplexity() > 3)
-        tentCostBash = actionCost(current.getCurrentCell(), neighbor.getCurrentCell(), True) + neighbor.neighbors[localDir].heuristic
-        tentCostDrive = actionCost(current.getCurrentCell(), neighbor.getCurrentCell(), False) + neighbor.heuristic
-        if tentCostBash < tentCostDrive
-            return True
-    else
+# this function tests if a given path of Nodes has a start Node
+# INPUT -> (list of Nodes) path
+# OUTPUT -> (boolean)
+def pathHasStart(path):
+        for node in path:
+            if node.getCell().getIsStart():
+                return True
         return False
 
+if __name__ == "__main__":
+	if len(sys.argv) != 3:
+		print "Structure is: python astar.py filename.txt <heuristic function integer>"
+	else:
+		filename = sys.argv[1]
+		heuristic = sys.argv[2]
 
-
-
-
-def actionCost(currentCell, moveCell, isBash):
-    localDir = findDirection(currentCell, moveCell)
-    turningCost = turningCalc(localDir)
-
-    if isBash
-        return turningCost + moveCell.neighbors[localDir].getComplexity() + 3
-    else
-        return turningCost + moveCell.getComplexity()
-
-
-
-                
+		the_world = World(open(filename, "r"), heuristic)
+		visited = AStar2(the_world)
