097c6aebb5519df0c04cc63adc61f0841fb847af
jlzizmor <jlzizmor@wpi.edu>
2015-09-13 17:49:23 -0400

    changed class name

--- Assignment2/Chromosome.py
+++ Assignment2/Chromosome.py
@@ -1,6 +1,6 @@
 from abc import ABCMeta, abstractmethod
 
-class Abstract(object):
+class Chromosome(object):
     __metaclass__ = ABCMeta
 
     # This method initializes the chromosome for generation 0
