67a9d58e993cda143e90529b51f222d213e23691
jlzizmor <jlzizmor@wpi.edu>
2015-09-13 17:47:41 -0400

    added comments

--- Assignment2/Chromosome.py
+++ Assignment2/Chromosome.py
@@ -3,24 +3,27 @@ from abc import ABCMeta, abstractmethod
 class Abstract(object):
     __metaclass__ = ABCMeta
 
+    # This method initializes the chromosome for generation 0
     @abstractmethod
     def initialize(self):
     	pass
 
+    # This method checks if a chromosome is legal
     @abstractmethod
     def checkLegality(self):
     	pass
 
+    # This method does a crossover for multiple chromosomes
     @abstractmethod
     def crossover(self):
     	pass
 
+    # this function evaluates the fitness of a chromosome
     @abstractmethod
     def fitness(self):
+    	pass
 
+    # this funcction mutates a chromosome
     @abstractmethod
     def mutate(self):
     	pass
-
-if __name__ == "__name__":
-	abbbb = Abstract()
