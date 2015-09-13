811a1f80cc4b6909a97882b046bb494c7a4da67a
jlzizmor <jlzizmor@wpi.edu>
2015-09-13 17:54:41 -0400

    changed from abstract class to class

--- Assignment2/Chromosome.py
+++ Assignment2/Chromosome.py
@@ -1,29 +1,23 @@
-from abc import ABCMeta, abstractmethod
-
 class Chromosome(object):
-    __metaclass__ = ABCMeta
+    def __init__(self):
+    	pass
 
     # This method initializes the chromosome for generation 0
-    @abstractmethod
     def initialize(self):
     	pass
 
     # This method checks if a chromosome is legal
-    @abstractmethod
     def checkLegality(self):
     	pass
 
     # This method does a crossover for multiple chromosomes
-    @abstractmethod
     def crossover(self):
     	pass
 
     # this function evaluates the fitness of a chromosome
-    @abstractmethod
     def fitness(self):
     	pass
 
     # this funcction mutates a chromosome
-    @abstractmethod
     def mutate(self):
     	pass
