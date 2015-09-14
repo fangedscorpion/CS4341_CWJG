f6d971fbd536f2ff68b4da39d2ca3d73944100b6
Chas Frick <cjfrick@wpi.edu>
2015-09-13 21:02:11 -0400

    Updated for num seconds

--- Assignment2/ga.py
+++ Assignment2/ga.py
@@ -5,10 +5,11 @@ if __name__ == "__main__":
     debug_mode = 1
 
     if len(sys.argv) != 3:
-        print "Structure is: python ga.py filename.txt puzzleNum"
+        print "Structure is: python ga.py filename.txt puzzleNum secondsToRun"
     else:
         filename = sys.argv[1]
         puzzleNum = int(sys.argv[2])
+        secondsToRun = int(sys.argv[3])
 
         the_list = ListParser(open(filename, "r"), puzzleNum)
 
