04ec6007a68f94b4215af60927b1904a5fa0e132
Chas Frick <cjfrick@wpi.edu>
2015-09-14 19:56:02 -0400

    Preliminary dash off of mutate. Still trying to understand what I did

--- Assignment2/Puzzle2.py
+++ Assignment2/Puzzle2.py
@@ -1,4 +1,5 @@
 from Chromosome import *
+import random as rand
 class Puzzle2(Chromosome):
 
     def __init__(self, bin1List, bin2List, bin3List, generation):
@@ -7,9 +8,11 @@ class Puzzle2(Chromosome):
         self.bin2 = bin2List
         self.bin3 = bin3List
 
+        self.mutationThreshold = 8 # 8% for each digit
+
     # This method initializes the Puzzle for generation 0
     def initialize(self):
-        pass
+        super(Puzzle2, self).__init__(0)
 
     # This method checks if a Puzzle is legal
     def checkLegality(self):
@@ -23,9 +26,68 @@ class Puzzle2(Chromosome):
     def fitness(self):
         pass
 
+    def getValidMutatedNumber(self):
+        # Consult dictionary here
+        return rand.randint(1, 50)
+
+    def fixChild(self, listOfChangedNums):
+        print listOfChangedNums
+
+        for x in range(0, len(listOfChangedNums)-1):
+            # Generate another rand int in size of changed list
+            ind1 = rand.randint(0, len(listOfChangedNums)-1)
+            ind2 = rand.randint(0, len(listOfChangedNums)-1)
+            #Swap values
+            if(listOfChangedNums[ind1][0] == 1):
+                self.bin1[listOfChangedNums[ind2][1]] = listOfChangedNums[ind2][2]
+            elif(listOfChangedNums[ind1][0] == 2):
+                self.bin2[listOfChangedNums[ind2][1]] = listOfChangedNums[ind2][2]
+            elif(listOfChangedNums[ind1][0] == 3):
+                self.bin3[listOfChangedNums[ind2][1]] = listOfChangedNums[ind2][2]
+
+            if(listOfChangedNums[ind2][0] == 1):
+                self.bin1[listOfChangedNums[ind1][1]] = listOfChangedNums[ind1][2]
+            elif(listOfChangedNums[ind2][0] == 2):
+                self.bin2[listOfChangedNums[ind1][1]] = listOfChangedNums[ind1][2]
+            elif(listOfChangedNums[ind2][0] == 3):
+                self.bin3[listOfChangedNums[ind1][1]] = listOfChangedNums[ind1][2]
+
+            del listOfChangedNums[ind1]
+            del listOfChangedNums[ind2]
+
+        if(len(listOfChangedNums) == 1):
+            if(listOfChangedNums[0][0] == 1):
+                self.bin1[listOfChangedNums[0][1]] = listOfChangedNums[0][2]
+            elif(listOfChangedNums[0][0] == 2):
+                self.bin2[listOfChangedNums[0][1]] = listOfChangedNums[0][2]
+            elif(listOfChangedNums[0][0] == 3):
+                self.bin3[listOfChangedNums[0][1]] = listOfChangedNums[0][2]
+
     # this funcction mutates a Puzzle
+    # It goes through all three at once and mutates if the number is between 1 and 8
+    # If so it gets added to spareNums as a tuple with the changed index and the bin #
+    # The fixChild class then makes the kid legal
     def mutate(self):
-        pass
+        spareNums = []
+        for x in range(0,len(self.bin1)):
+            l1Rand = rand.randint(1,100)
+            l2Rand = rand.randint(1,100)
+            l3Rand = rand.randint(1,100)
+
+
+            if(l1Rand <= self.mutationThreshold):
+                spareNums.append([1, x, self.bin1[x]])
+                self.bin1[x] = self.getValidMutatedNumber()
+
+            if(l2Rand <= self.mutationThreshold):
+                spareNums.append([2, x, self.bin2[x]])
+                self.bin2[x] = self.getValidMutatedNumber()
+
+            if(l3Rand <= self.mutationThreshold):
+                spareNums.append([3, x, self.bin3[x]])
+                self.bin3[x] = self.getValidMutatedNumber()
+
+        self.fixChild(spareNums)
 
     def getBin1(self):
         return self.bin1
@@ -51,6 +113,10 @@ if __name__ == '__main__':
     print par1
     print par2
     print par3
+    print par4
+
+    par4.mutate()
+
     print par4
 
     print par1.getGeneration()
