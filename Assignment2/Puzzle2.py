e4304f713253bab3467a65fc8e998fdf2db66b1f
Jacob Zizmor <jlzizmor@wpi.edu>
2015-09-14 21:32:29 -0400

    fixed syntax

--- Assignment2/Puzzle2.py
+++ Assignment2/Puzzle2.py
@@ -1,6 +1,7 @@
 from Chromosome import *
 import random as rand
 
+
 class Puzzle2(Chromosome):
 
     def __init__(self, bin1List, bin2List, bin3List, generation):
@@ -9,7 +10,7 @@ class Puzzle2(Chromosome):
         self.bin2 = bin2List
         self.bin3 = bin3List
 
-        self.mutationThreshold = 8 # 8% for each digit
+        self.mutationThreshold = 8  # 8% for each digit
 
     # This method initializes the Puzzle for generation 0
     def initialize(self):
@@ -26,8 +27,8 @@ class Puzzle2(Chromosome):
     # this function evaluates the fitness of a Puzzle
     # fitness = product(bin1) + sum(bin2)
     def fitness(self):
-        bProd = 1;
-        bSum = 0;
+        bProd = 1
+        bSum = 0
 
         # because  for a Puzzle2 to be legal, all bins must be the same size
         # therefore, 1 for loop can be used
@@ -35,7 +36,7 @@ class Puzzle2(Chromosome):
             bProd *= self.bin1[j]
             bSum += self.bin2[j]
 
-        return (bProd + bSum)/2
+        return (bProd + bSum) / 2
 
     def getValidMutatedNumber(self):
         # Consult dictionary here
@@ -44,24 +45,30 @@ class Puzzle2(Chromosome):
     def fixChild(self, listOfChangedNums):
         print listOfChangedNums
 
-        for x in range(0, len(listOfChangedNums)-1):
+        for x in range(0, len(listOfChangedNums) - 1):
             # Generate another rand int in size of changed list
-            ind1 = rand.randint(0, len(listOfChangedNums)-1)
-            ind2 = rand.randint(0, len(listOfChangedNums)-1)
-            #Swap values
+            ind1 = rand.randint(0, len(listOfChangedNums) - 1)
+            ind2 = rand.randint(0, len(listOfChangedNums) - 1)
+            # Swap values
             if(listOfChangedNums[ind1][0] == 1):
-                self.bin1[listOfChangedNums[ind2][1]] = listOfChangedNums[ind2][2]
+                self.bin1[listOfChangedNums[ind2][1]] = listOfChangedNums[
+                    ind2][2]
             elif(listOfChangedNums[ind1][0] == 2):
-                self.bin2[listOfChangedNums[ind2][1]] = listOfChangedNums[ind2][2]
+                self.bin2[listOfChangedNums[ind2][1]] = listOfChangedNums[
+                    ind2][2]
             elif(listOfChangedNums[ind1][0] == 3):
-                self.bin3[listOfChangedNums[ind2][1]] = listOfChangedNums[ind2][2]
+                self.bin3[listOfChangedNums[ind2][1]] = listOfChangedNums[
+                    ind2][2]
 
             if(listOfChangedNums[ind2][0] == 1):
-                self.bin1[listOfChangedNums[ind1][1]] = listOfChangedNums[ind1][2]
+                self.bin1[listOfChangedNums[ind1][1]] = listOfChangedNums[
+                    ind1][2]
             elif(listOfChangedNums[ind2][0] == 2):
-                self.bin2[listOfChangedNums[ind1][1]] = listOfChangedNums[ind1][2]
+                self.bin2[listOfChangedNums[ind1][1]] = listOfChangedNums[
+                    ind1][2]
             elif(listOfChangedNums[ind2][0] == 3):
-                self.bin3[listOfChangedNums[ind1][1]] = listOfChangedNums[ind1][2]
+                self.bin3[listOfChangedNums[ind1][1]] = listOfChangedNums[
+                    ind1][2]
 
             del listOfChangedNums[ind1]
             del listOfChangedNums[ind2]
@@ -80,11 +87,10 @@ class Puzzle2(Chromosome):
     # The fixChild class then makes the kid legal
     def mutate(self):
         spareNums = []
-        for x in range(0,len(self.bin1)):
-            l1Rand = rand.randint(1,100)
-            l2Rand = rand.randint(1,100)
-            l3Rand = rand.randint(1,100)
-
+        for x in range(0, len(self.bin1)):
+            l1Rand = rand.randint(1, 100)
+            l2Rand = rand.randint(1, 100)
+            l3Rand = rand.randint(1, 100)
 
             if(l1Rand <= self.mutationThreshold):
                 spareNums.append([1, x, self.bin1[x]])
@@ -110,12 +116,12 @@ class Puzzle2(Chromosome):
         return self.bin3
 
     def __repr__(self):
-        return ("Bin1: " + str(self.bin1) + "\nBin2: " + \
+        return ("Bin1: " + str(self.bin1) + "\nBin2: " +
                 str(self.bin2) + "\nBin3: " + str(self.bin3) + "\n")
 
 if __name__ == '__main__':
     from Puzzle2 import Puzzle2
-    a = [1,0,-6,-9.9,8,4.5,3,3.8,2.5]
+    a = [1, 0, -6, -9.9, 8, 4.5, 3, 3.8, 2.5]
     par1 = Puzzle2(a[0:3], a[3:6], a[6:9], 0)
     par2 = Puzzle2(a[3:6], a[0:3], a[6:9], 0)
     par3 = Puzzle2(a[6:9], a[3:6], a[0:3], 0)
