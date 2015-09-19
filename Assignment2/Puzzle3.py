09042d642ca19f99820e66dd85f312ad5714aa2f
jlzizmor <jlzizmor@wpi.edu>
2015-09-18 21:50:23 -0400

    wrote and tested init()

--- Assignment2/Puzzle3.py
+++ Assignment2/Puzzle3.py
@@ -1,16 +1,29 @@
 import random as rand
+import copy
+from Piece import Piece
+from Chromosome import Chromosome
 
-class Puzzle3(object):
+class Puzzle3(Chromosome):
     def __init__(self, gen):
         super(Puzzle3, self).__init__(gen)
         self.mutationThreshold = 50  # Percentage of 100 for each digit
+        self.height = -1
+        self.towerList = []
 
     # This method initializes the chromosome for generation 0
     # jake
     def initialize(self, masterList):
         super(Puzzle3, self).__init__(0)
-        self.towerList = XXXX # Ind 0 = Bottom 
-        # Height = len(towerList)
+        size = rand.randint(2, len(masterList))
+
+        new_masterList = copy.copy(masterList)
+
+        for j in range(0, size):
+            a_piece = new_masterList[rand.randint(0, len(new_masterList) - 1)]
+            new_masterList.remove(a_piece)
+            self.towerList.append(a_piece)
+
+        self.height = len(self.towerList)
 
     # This method checks if a chromosome is legal
     # jake
@@ -58,3 +71,13 @@ class Puzzle3(object):
 
     def getGeneration(self):
         return self.generation
+
+if __name__ == "__main__":
+    a = [1,2,3,4,5,6,7,8,9]
+    alpha = Puzzle3(0)
+    beta = Puzzle3(0)
+    alpha.initialize(a)
+    beta.initialize(a)
+
+    print alpha.towerList
+    print beta.towerList
