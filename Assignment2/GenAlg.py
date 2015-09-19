b0574cb8b124036ae40ebf9cf0c4e149b9d910e9
wsbarnard <wsbarnard@wpi.edu>
2015-09-18 21:06:52 -0400

    changing GenAlg to make it work with functions that run on individual chromosomes

--- Assignment2/GenAlg.py
+++ Assignment2/GenAlg.py
@@ -1,61 +1,76 @@
+import random
+
+def geneticAlgorithm(puzzle, validValues):
+    chromosomes = makeChromes(puzzle)
+    gen = makeNodes(chromosomes)
+
+    while(time ldakjflasdj):
+        evalGen(gen)
+        mate(gen)
+        mutate(gen)
+        makeValid(gen)
 
 
-def geneticAlgorithm(puzzle):
-	puzzle.initFirstGen()
+# creates 500 chromosomes (hopefully unique, though not explicitly)
+def makeChromes(puzzle, validValues): 
+    chromes = []
+    for i in range(500):
+        chromes[i] = Puzzle2([], [], [], 0)
+        chromes[i].initialize(validValues)
 
-	while(time ldakjflasdj):
-		puzzle.EvalGen()
-		puzzle.gen = Mate(puzzle.Gen)
+    return chromes
 
+def makeNodes(chromosomes):
+    nodes = []
+    for i in len(chromosomes):
+        nodes[i] = node(chromosomes, 0, 0)
 
+    return nodes
 
-def Mate(gen): #make the new generation, new Gen is not mutated or valid 
-	#double check the ABOVE comment about mutation and validity
-        tempGen = gen
-	preGen = []
+# 
+def evalGen(gen):
+    netFitness = 0
+    netPercent = 0
+    for node in len(gen):
+        node.fitness = node.chromosome.fitness()
+        netFitness += node.fitness
 
-	while(tempGen != [])
-		index1 = rand(0, len(tempGen)-1) 
-		tempGen.remove(index1)
-		chrome1 = tempGen[index1] 
-
-		index2 = rand(0, len(tempGen)-1)
-		tempGen.remove(index2)		
-		chrome2 = tempGen[index2]
-
-		index3 = rand(0, len(tempGen)-1)
-		tempGen.remove(index3)		
-		chrome3 = tempGen[index3]
-
-		index4 = rand(0, len(tempGen)-1)
-		tempGen.remove(index4)		
-		chrome4 = tempGen[index4]
-
-		if chrome1.fitness > chrome2.fitness:
-			win1 = chrome1
-			tempGen.append(chrome2)
-		else:
-			win1 = chrome2
-			tempGen.append(chrome1)
-
-		if chrome3.fitness > chrome4.fitness:
-			win2 = chrome3
-			tempGen.append(chrome4)
-		else:
-			win2 = chrome4
-			tempGen.append(chrome3)
-
-                #should we be adding to a compiled list of the entire generation or just take the 2 crossed over?
-		preGen = crossOver(win1, win2)
-
-	for chrome in preGen:
-		chrome.mutate()
-		chrome.makeValid() 
-
-	return preGen
+    for node in len(gen):
+        percentFitness = (node.fitness/netFitness)*100
+        netPercent += percentFitness
+        node.percentBound = netPercent
 
 
-def mutate(gen):
+
+
+def mate(gen): #make the new generation, new Gen is not mutated or valid 
+    for i in len(gen):
+        point1 = random.random()*100
+        point2 = random.random()*100
+        for j in len(gen):
+            if (point1 > gen[j-1]) and (point1 < gen[j]):
+                parentA = gen[j]
+            if (point2 > gen[j-1]) and (point2 < gen[j]):
+                parentB = gen[j]
+
+        
+
+
+    
+
+
+class node():
+
+    def __init__(self, chromosome, fitness, percentBound):
+        self.chromosome = chromosome
+        self.fitness = fitness
+        self.percentBound = percentBound
+
+
+
+
+
+
 
 
 
