89327d36be46fce5a2030508d44b4389aaeef5c2
jlzizmor <jlzizmor@wpi.edu>
2015-09-18 22:23:23 -0400

    <(','<)

--- Assignment2/Piece.py
+++ Assignment2/Piece.py
@@ -5,15 +5,15 @@ class Piece:
     # strength is a number (float or int) # of pieces can be stacked on
     # cost is expense of piece (number float or int)
     def __init__(self, pieceType, width, strength, cost, ID):
-        self.type = pieceType 
+        self.type = pieceType
         self.width = width
         self.strength = strength
         self.cost = cost
         self.ID = ID
 
     def __repr__(self):
-        return (str(self.type) + "," + str(self.width) + "," + str(self.strength) + \
-            "," + str(self.cost))
+        return (str(self.type) + "," + str(self.width) + "," + str(self.strength) +
+                "," + str(self.cost))
 
     def __eq__(self, other):
         return (self.type.lower() == other.getType().lower() and
@@ -52,17 +52,18 @@ class Piece:
         return self.type.lower() == "wall"
 
     def getDictKey(self):
-        return (self.type[0]+str(self.width)+str(self.strength)+str(self.cost)) #unhashable otherwise
+        # unhashable otherwise
+        return (self.type[0] + str(self.width) + str(self.strength) + str(self.cost))
 
 if __name__ == '__main__':
-    d1 = Piece("Door", 5,3,2, 0)
-    w1 = Piece("Wall", 5,5,1, 1)
-    w2 = Piece("Wall", 4,3,1, 2)
-    d2 = Piece("Door", 3,5,2, 3)
-    w3 = Piece("Wall", 3,3,2, 4)
-    l1 = Piece("Lookout",2,2,3, 5)
-    l2 = Piece("Lookout",3,1,2, 6)
-    l3 = Piece("Lookout",3,1,2, 6)
+    d1 = Piece("Door", 5, 3, 2, 0)
+    w1 = Piece("Wall", 5, 5, 1, 1)
+    w2 = Piece("Wall", 4, 3, 1, 2)
+    d2 = Piece("Door", 3, 5, 2, 3)
+    w3 = Piece("Wall", 3, 3, 2, 4)
+    l1 = Piece("Lookout", 2, 2, 3, 5)
+    l2 = Piece("Lookout", 3, 1, 2, 6)
+    l3 = Piece("Lookout", 3, 1, 2, 6)
 
     assert d1.isLookout() == False
     assert l1.isLookout() == True
