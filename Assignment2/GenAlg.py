11be87a8fe0d01322de712a38d8103e7af60e479
wsbarnard <wsbarnard@wpi.edu>
2015-09-20 16:02:36 -0400

    more work on GenAlg git status

--- Assignment2/GenAlg.py
+++ Assignment2/GenAlg.py
@@ -3,6 +3,7 @@ import random
 def geneticAlgorithm(puzzle, validValues):
     chromosomes = makeChromes(puzzle)
     gen = makeNodes(chromosomes)
+    masterDict = makeDict(validValues)
 
     while(time ldakjflasdj):
         evalGen(gen)
@@ -27,7 +28,13 @@ def makeNodes(chromosomes):
 
     return nodes
 
-# 
+def makeDict(values):
+    newDict = []
+    for i in len(values):
+        newDict
+
+# defines the fitness of each node and the % of the net fitness which will be used for 
+# mating
 def evalGen(gen):
     netFitness = 0
     netPercent = 0
@@ -44,14 +51,36 @@ def evalGen(gen):
 
 
 def mate(gen): #make the new generation, new Gen is not mutated or valid 
+    preGen = []
+
     for i in len(gen):
         point1 = random.random()*100
         point2 = random.random()*100
         for j in len(gen):
-            if (point1 > gen[j-1]) and (point1 < gen[j]):
+            if (point1 > gen[j-1].percentBound) and (point1 < gen[j].percentBound):
                 parentA = gen[j]
-            if (point2 > gen[j-1]) and (point2 < gen[j]):
+            if (point2 > gen[j-1].percentBound) and (point2 < gen[j].percentBound):
                 parentB = gen[j]
+            while (parentA is parentB): # if parents A and B are the same rerun B, no clones
+                point2 = random.random()*100
+                if (point2 > gen[j-1].percentBound) and (point2 < gen[j].percentBound):
+                parentB = gen[j]
+
+            preGen += parentA.chromosome.crossover(parentB.chromosome)
+
+    gen = preGen
+
+def mutate(gen):
+    mutGen = []
+
+    for node in gen:
+        node.chromosome = node.chromosome.mutate()
+
+def checkValid(gen):
+
+
+        
+
 
         
 
