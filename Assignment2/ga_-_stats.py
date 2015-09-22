34eca7739cd099c7abfba852272c63490650da62
jlzizmor <jlzizmor@wpi.edu>
2015-09-22 00:13:51 -0400

    loops through all puzzles

--- Assignment2/ga_-_stats.py
+++ Assignment2/ga_-_stats.py
@@ -6,24 +6,21 @@ if __name__ == "__main__":
     run_time = 15
     puzzleNum = 2
     sample_size = 10
-    filename = "sample_puzzle2_list.txt"
-    fileWrite = open("popTest_-_Puzzle2.csv", "w+")
+    filename1 = "sample_puzzle"
+    filename2 = "_list.txt"
 
-    the_list_parser = ListParser(open(filename, "r"), puzzleNum)
 
-    if(debug_mode == 1):
-        print the_list_parser.getList()
-        print
-        print the_list_parser.getDictionary()
-        print
-        print the_list_parser.getTarget()
 
-    for pop_size in range(2, 8, 2):
-        fileWrite.write(str(pop_size) + ",")
-        for j in range(0, sample_size):
-            # best = geneticAlgorithm(puzzleNum, the_list_parser.getList(), run_time, the_list_parser.getDictionary(), pop_size)
-            # fileWrite.write(str(best.fitness()))
-            fileWrite.write(str(j) + ",")
+    for puzzleNum in range(1, 4):
+        fileWrite = open("popTest_-_Puzzle" + str(puzzleNum) + ".csv", "w+")
+        the_list_parser = ListParser(open(filename1 + str(puzzleNum) + filename2, "r"), puzzleNum)
 
-        fileWrite.write("\n")
-    fileWrite.close()
+        for pop_size in range(2, 8, 2):
+            fileWrite.write(str(pop_size) + ",")
+            for j in range(0, sample_size):
+                # best = geneticAlgorithm(puzzleNum, the_list_parser.getList(), run_time, the_list_parser.getDictionary(), pop_size)
+                # fileWrite.write(str(best.fitness()))
+                fileWrite.write("3,") # swap with above when geneticAlgorithm is repaired
+
+            fileWrite.write("\n")
+        fileWrite.close()
