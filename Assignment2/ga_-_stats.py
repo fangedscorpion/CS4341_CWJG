85056cf7b44f4e8f9236c740080ea7b17aa90d4e
jlzizmor <jlzizmor@wpi.edu>
2015-09-24 02:01:11 -0400

    fixed .csv formatting

--- Assignment2/ga_-_stats.py
+++ Assignment2/ga_-_stats.py
@@ -22,19 +22,9 @@ if __name__ == "__main__":
     puzzleNumList = [1]  # Add in 2 or 3 for more puzzles
 
     for puzzleNum in puzzleNumList:
-        if (puzzleNum == 1) and (j == 0):
-            the_list_parser = ListParser(
-                open(filename1 + str(puzzleNum) + filename2, "r"), puzzleNum)
-            j += 1
-            fileWrite = open("popTest_-_Puzzle" + str(puzzleNum) + ".csv", "w+")
-        elif (puzzleNum == 1) and (j == 1):
-            the_list_parser = ListParser(
-                open(filename1 + str(puzzleNum) + "-1" + filename2, "r"), puzzleNum)
-            fileWrite = open("popTest_-_Puzzle" + str(puzzleNum) + "-1" + ".csv", "w+")
-        else:
-            the_list_parser = ListParser(
-                open(filename1 + str(puzzleNum) + filename2, "r"), puzzleNum)
-            fileWrite = open("popTest_-_Puzzle" + str(puzzleNum) + ".csv", "w+")
+        the_list_parser = ListParser(
+            open(filename1 + str(puzzleNum) + filename2, "r"), puzzleNum)
+        fileWrite = open("popTest_-_Puzzle" + str(puzzleNum) + ".csv", "w+")
 
         print "puzzle: ", puzzleNum
         fileWrite = open("popTest_-_Puzzle" + str(puzzleNum) + ".csv", "w+")
@@ -42,14 +32,31 @@ if __name__ == "__main__":
         for pop_size in range(startPopSize, endingPopSize, popStepSize):
             print "\n"
             print "pop_size: ", pop_size
-            fileWrite.write(str(pop_size) + ",")
             for j in range(0, numberOfRuns):
                 print "-" * 20
                 print "sample: ", j
                 (answer, gens) = geneticAlgorithm(puzzleNum, the_list_parser.getList(), runTimeSeconds, the_list_parser.getDictionary(
                 ), pop_size, mutatePerc, the_list_parser.getTarget(), geneticsType)
                 print answer, answer.fitness(the_list_parser.getDictionary(), the_list_parser.getTarget())
-                fileWrite.write(str(answer.fitness(
-                    the_list_parser.getDictionary(), the_list_parser.getTarget())) + ",")
-            fileWrite.write("\n")
+                fileWrite.write(str(pop_size) + "," + str(answer.fitness(
+                    the_list_parser.getDictionary(), the_list_parser.getTarget())) + "," + str(answer.getGeneration()) + "," + str(gens) + "\n")
         fileWrite.close()
+
+        if puzzleNum == 1:
+            the_list_parser = ListParser(
+                open(filename1 + str(puzzleNum) + "-1" + filename2, "r"), puzzleNum)
+            fileWrite = open(
+                "popTest_-_Puzzle" + str(puzzleNum) + "-1" + ".csv", "w+")
+
+            for pop_size in range(startPopSize, endingPopSize, popStepSize):
+                print "\n"
+                print "pop_size: ", pop_size
+                for j in range(0, numberOfRuns):
+                    print "-" * 20
+                    print "sample: ", j
+                    (answer, gens) = geneticAlgorithm(puzzleNum, the_list_parser.getList(), runTimeSeconds, the_list_parser.getDictionary(
+                    ), pop_size, mutatePerc, the_list_parser.getTarget(), geneticsType)
+                    print answer, answer.fitness(the_list_parser.getDictionary(), the_list_parser.getTarget())
+                    fileWrite.write(str(pop_size) + "," + str(answer.fitness(
+                        the_list_parser.getDictionary(), the_list_parser.getTarget())) + "," + str(answer.getGeneration()) + "," + str(gens) + "\n")
+            fileWrite.close()
