73769cd83847ad245e21d0b17d3efd2d42760b43
jlzizmor <jlzizmor@wpi.edu>
2015-09-25 19:53:53 -0400

    main function to assignment specs

--- Assignment2/ga.py
+++ Assignment2/ga.py
@@ -1,26 +1,35 @@
 import sys
 from ListParser import ListParser
-from geneticAlgo import geneticAlgo
+from GenAlg2 import geneticAlgorithm
 
 if __name__ == "__main__":
-    debug_mode = 1
+    debug_mode = 0
 
-    if len(sys.argv) != 4:
+    if len(sys.argv) < 4:
         print "Structure is: python ga.py filename.txt puzzleNum secondsToRun"
     else:
         filename = sys.argv[1]
         puzzleNum = int(sys.argv[2])
         secondsToRun = int(sys.argv[3])
 
+        if (len(sys.argv) == 5):
+            mode = int(sys.argv[4])
+        else:
+            mode = 0
+
         the_list_parser = ListParser(open(filename, "r"), puzzleNum)
+        mutationRate = 10
+        population = 50
 
         if(debug_mode == 1):
-        	print the_list_parser.getList()
-        	print
-        	print the_list_parser.getDictionary()
-        	print
-        	print the_list_parser.getTarget()
+            print the_list_parser.getList()
+            print
+            print the_list_parser.getDictionary()
+            print
+            print the_list_parser.getTarget()
 
-        # geneticAlgo(the_list_parser.getList(), secondsToRun, the_list_parser.getDictionary())
-
-
+        answer = geneticAlgorithm(puzzleNum, the_list_parser.getList(), secondsToRun, the_list_parser.getDictionary(
+        ), population, mutationRate, the_list_parser.getTarget(), mode)
+        print "\nBest solution: \n", answer[0]
+        print "\nScore:\n", answer[0].fitness(the_list_parser.getDictionary(), the_list_parser.getTarget())
+        print "\nTotal generations: \n", answer[1]
