9c93a86b052b143940ce6e2c7b04f473d0d9da1a
Jacob Zizmor <jlzizmor@wpi.edu>
2015-10-10 16:02:24 -0400

    Wrote main run file for Rollouts

---
+++ Final Project/DateTimeCustom.py
@@ -0,0 +1,27 @@
+from datetime import datetime
+import copy
+
+
+class DateTimeCustom(object):
+    # hour recorded in military time
+
+    def __init__(self, _day, _hour, _minute):
+        self.dt = datetime.now()
+        self.dt = self.dt.replace(day=_day, hour=_hour, minute=_minute)
+
+    # creates a DateTimeCustom object from the current time
+    def makeObj(self):
+        temp = datetime.now()
+        return copy.deepcopy(DateTimeCustom(temp.day, temp.hour, temp.minute))
+
+    # returns other >= self
+    # in this case, self is the goal time
+    def greaterEqualTo(self):
+        # print datetime.now().second, datetime.now() >= self.dt, datetime.now()
+        return datetime.now() >= self.dt
+
+if __name__ == "__main__":
+    endTime = DateTimeCustom(10, 15, 49)
+
+    while not (endTime.greaterEqualTo()):
+        print endTime.greaterEqualTo()
---
+++ Final Project/rolloutsMain.py
@@ -0,0 +1,20 @@
+from CasinoBJTable import CasinoBJTable
+from StaticBJLogger import StaticBJLogger
+from datetime import datetime
+from DateTimeCustom import DateTimeCustom
+
+# change this number on each computer!
+StaticBJLogger.init(0)
+
+# Fill in (date, hour, minute)
+# date is the calendar date
+# hour is the hour in military time
+endTime = DateTimeCustom(10, 15, 54)
+
+while not endTime.greaterEqualTo():
+    # print endTime.greaterEqualTo()
+    table = CasinoBJTable(6, 1)
+    table.initPlayers()
+    table.playRound()
+
+    del table
