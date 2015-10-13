4e6199e836ec7d3ae886c4ab480aa9e11c807e2e
wsbarnard <wsbarnard@wpi.edu>
2015-10-13 02:02:18 -0400

    finished CCPlayer, need to write ROPlayer

---
+++ Final Project/CCPlayer.py
@@ -0,0 +1,28 @@
+from Player import Player
+from Card import Card
+from Hand import Hand
+from CC import CC
+
+class CCPlayer(Player):
+
+    def __init__(self, idName, bankAccountStart, numDecks):
+        super(idName, bankAccountStart, numDecks)
+        self.myCC = CC(numDecks)
+
+    def reset(self):
+    	myCC.reset()
+
+    def countCard(self, card):
+    	self.myCC.update(card)
+
+   	def play(self):
+
+   		if (self.getHands()[0].getHandValue() + self.myCC.getEV() > 21):
+   			#stay
+   			return False
+   		else:
+   			#hit
+   			return True
+
+
+
---
+++ Final Project/ROPlayer.py
@@ -0,0 +1,11 @@
+from Player import Player
+from Card import Card
+
+class ROPlayer(Player):
+
+    def __init__(self, idName, bankAccountStart, numDecks):
+        super(idName, bankAccountStart, numDecks)
+
+
+   	def playRO(self, Hand, DealerShow, ROref):
+   		
